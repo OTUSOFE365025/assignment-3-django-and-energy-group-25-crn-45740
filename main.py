2611637cf18dd9e21b29fd4d6e4e37fdda4b7b6c
zachmatt02 <zachmatasic@gmail.com>
2025-11-07 17:00:36 -0500

    Init the DB

--- main.py
+++ main.py
@@ -24,9 +24,27 @@ from db.models import *
 ############################################################################
 """ Replace the code below with your own """
 
-# Seed a few users in the database
-User.objects.create(name='Dan')
-User.objects.create(name='Robert')
+# Seed a few Products in the database
+Product.objects.create(UPC='12345', Name='Coffee', Price=8.32)
+Product.objects.create(UPC='67890', Name='Muffin', Price=2.50)
+Product.objects.create(UPC='11111', Name='Tea', Price=3.99)
+Product.objects.create(UPC='22222', Name='Sandwich', Price=6.75)
+Product.objects.create(UPC='33333', Name='Bagel', Price=2.99)
+Product.objects.create(UPC='44444', Name='Croissant', Price=4.25)
+Product.objects.create(UPC='55555', Name='Juice', Price=5.50)
+Product.objects.create(UPC='66666', Name='Donut', Price=1.99)
+Product.objects.create(UPC='77777', Name='Cookie', Price=2.25)
+Product.objects.create(UPC='88888', Name='Water', Price=1.50)
+Product.objects.create(UPC='99999', Name='Cabbage', Price=2.50)
+Product.objects.create(UPC='12222', Name='Tea', Price=1.50)
+Product.objects.create(UPC='12333', Name='Milk', Price=5.00)
+Product.objects.create(UPC='12344', Name='Carrot', Price=2.25)
+Product.objects.create(UPC='13333', Name='Lettuce', Price=4.15)
+Product.objects.create(UPC='13244', Name='Apple', Price=1.50)
+Product.objects.create(UPC='14223', Name='Egg', Price=1.25)
+Product.objects.create(UPC='20833', Name='Chips', Price=3.00)
+Product.objects.create(UPC='21334', Name='Cheese', Price=3.25)
 
-for u in User.objects.all():
-    print(f'ID: {u.id} \tUsername: {u.name}')
+
+for u in Product.objects.all():
+    print(f'ID: {u.id} \tProductname: {u.Name}')
