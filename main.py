c3aaec0bf38dca9cb7433dcc392c6702eea22eee
LilMactop17 <mohamed.tawfikomar@ontariotechu.net>
2025-11-10 18:56:30 -0500

    fixed ui

--- main.py
+++ main.py
@@ -6,14 +6,17 @@ these settings as is, and skip to START OF APPLICATION section below """
 
 # Turn off bytecode generation
 import sys
+
 sys.dont_write_bytecode = True
 
 # Import settings
 import os
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
 
 # setup django environment
 import django
+
 django.setup()
 
 # Import your models for use in your script
@@ -47,22 +50,18 @@ Product.objects.create(UPC='14223', Name='Egg', Price=1.25)
 Product.objects.create(UPC='20833', Name='Chips', Price=3.00)
 Product.objects.create(UPC='21334', Name='Cheese', Price=3.25)
 
-
 for u in Product.objects.all():
     print(f'ID: {u.id} \tProductname: {u.Name}')
 
     i = 3
 
-
-
 total = 0.00
-rowHeight =4
+rowHeight = 4
 
-def scan_product(): 
 
+def scan_product():
     global rowHeight
     global total
-
 
     scanned_upc = upc_var.get()
     product_obj = Product.objects.filter(UPC=scanned_upc).first()
@@ -71,44 +70,55 @@ def scan_product():
         prodPrice = product_obj.Price
         prodName = product_obj.Name
         total = total + prodPrice
-        #print(f"Found Product: {prodName} | Price: {prodPrice}")
-        receiptLineText = prodName + "\t\t\t$" + str(prodPrice) + "\t\t\t" + scanned_upc
-        receiptLine_label = tk.Label(root, text=receiptLineText)
+        # print(f"Found Product: {prodName} | Price: {prodPrice}")
+        receiptLineProduct_label = tk.Label(root, text=prodName)
+        receiptLinePrice_label = tk.Label(root, text=str(prodPrice))
+        receiptLineUPC_label = tk.Label(root, text=scanned_upc)
 
-        receiptLine_label.grid(row=rowHeight, column=0, padx=5, pady=5)
-        
+        receiptLineProduct_label.grid(row=rowHeight, column=0, padx=5, pady=5, sticky="w")
+        receiptLinePrice_label.grid(row=rowHeight, column=2, padx=5, pady=5, sticky="w")
+        receiptLineUPC_label.grid(row=rowHeight, column=1, padx=5, pady=5, sticky="w")
 
-        totalText = "TOTAL:\t\t\t\t\t\t$" + str(total)
-        totalLine_label = tk.Label(root, text=totalText)
-        totalLine_label.grid(row=rowHeight+ 1, column=0, padx=5, pady=5)
+        totalLine_label.config(text="$" + str(total))
 
-        rowHeight = rowHeight +1
+        totalText_Label.grid_forget()
+        totalLine_label.grid_forget()
 
-        
+        totalText_Label.grid(row=rowHeight + 1, column=0, padx=5, pady=5, sticky="w")
+        totalLine_label.grid(row=rowHeight + 1, column=2, padx=5, pady=5, sticky="w")
+
+        rowHeight = rowHeight + 1
+
+
 
     else:
         print(f"Error: No product found for UPC {scanned_upc}.")
 
+
 root = tk.Tk()
 root.title("Cash Register")
-root.geometry("500x800")
+root.geometry("600x800")
 
 upc_var = tk.StringVar()
-
-root.grid_columnconfigure(2, weight=1)
 
 upc_label = tk.Label(root, text="Enter UPC")
 upc_entry = tk.Entry(root, textvariable=upc_var)
 scan_button = tk.Button(root, text="Scan", command=scan_product)
-receiptHeader_label = tk.Label(root, text="Product\t\t\tPrice\t\t\tUPC")
-
+receiptHeaderProduct_label = tk.Label(root, text="Product")
+receiptHeaderPrice_label = tk.Label(root, text="Price")
+receiptHeaderUPC_label = tk.Label(root, text="UPC")
 
 upc_label.grid(row=1, column=0, padx=5, pady=5, sticky="w")
-upc_entry.grid(row=2, column=0, padx=5, pady=5, sticky="w")
-scan_button.grid(row=2, column=2, columnspan=2, pady=10, sticky="e")
-receiptHeader_label.grid(row=3, column=0, columnspan=5, padx=5, pady=5, sticky="w")
+upc_entry.grid(row=2, column=0, columnspan=2, padx=5, pady=5, sticky="ew")
+scan_button.grid(row=2, column=2, pady=10, sticky="e")
+
+receiptHeaderProduct_label.grid(row=3, column=0, padx=5, pady=5, sticky="w")
+receiptHeaderPrice_label.grid(row=3, column=2, padx=5, pady=5, sticky="w")
+receiptHeaderUPC_label.grid(row=3, column=1, padx=5, pady=5, sticky="w")
+
+totalText_Label = tk.Label(root, text="Total is")
+totalLine_label = tk.Label(root, text="$" + str(total))
 
 root.mainloop()
-
 
 
